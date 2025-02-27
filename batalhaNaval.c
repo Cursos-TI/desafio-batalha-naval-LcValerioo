0568eacbaac0c8e6ce793ea2ccb8432f20920445
Lucas Valério <126265243+LcValerioo@users.noreply.github.com>
2025-02-27 04:47:03 +0000

    implementação de mais dois barcos

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,6 +3,9 @@
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
+//Criação das constantes de tamanho da matriz
+#define LINHA 10
+#define COLUNA 10
 
 int main() {
     // Declarando as variáveis
@@ -14,7 +17,7 @@ int main() {
     printf("  ");
 
     // Impressão da linha com letras (A-J)
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < LINHA; i++) {
         printf("%2c ", linha[i]);
     }
     printf("\n");  // Corrigido para quebrar a linha corretamente
@@ -28,13 +31,23 @@ int main() {
     tabuleiro[5][7] = 3;
     tabuleiro[6][7] = 3;
     tabuleiro[7][7] = 3;
+    
+    // Posicionando o navio 3 (Diagonal)
+    tabuleiro[3][0] = 3;
+    tabuleiro[2][1] = 3;
+    tabuleiro[1][2] = 3;
+    
+    // Posicionando o navio 4 (Diagonal)
+    tabuleiro[9][3] = 3;
+    tabuleiro[8][2] = 3;
+    tabuleiro[7][1] = 3;
 
     // Impressão do tabuleiro
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < LINHA; i++) {
         // Impressão da coluna numerada
         printf("%2d", coluna[i]);
 
-        for (j = 0; j < 10; j++) {
+        for (j = 0; j < COLUNA; j++) {
             printf(" %d ", tabuleiro[i][j]);
         }
         printf("\n");  // Para formatar corretamente o tabuleiro
